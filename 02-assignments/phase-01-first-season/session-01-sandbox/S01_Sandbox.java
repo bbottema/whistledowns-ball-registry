2fde203080652d3fe654d9eb3c93ea7f995497ba
bbottema <benny@bennybottema.com>
2026-02-19 13:28:41 +0100

    Update S01_Sandbox.java to introduce variable types and data representation concepts in Java basics session.

--- 02-assignments/phase-01-first-season/session-01-sandbox/S01_Sandbox.java
+++ 02-assignments/phase-01-first-season/session-01-sandbox/S01_Sandbox.java
@@ -1,4 +1,17 @@
-// Session 01 - JShell Sandbox
+// Session 01 - Java basics in the JShell Sandbox
+
+/*
+ * So, here we are! This is the beginning of your first session. Make sure you read the README.md first.
+ *
+ * This session is about learning how to define a variable that can hold different types of data.
+ *
+ * In Java, everything that can hold or give a value is always preceded by a type. You can say it out loud the same way you declare a variable:
+ * "I'm declaring an 'integer' named 'age' and I'm giving it the value '21'" -> integer age = 21
+ *
+ * There are a few different types of data that variables can hold, such as integers, strings, and booleans. Don't worry about all the other types yet; in fact, with these three types
+ * you can already create everything you need to create a program. You can use integers to represent numbers, strings to represent text, and booleans to represent true/false values.
+ */
+
 // Copy these snippets into the JShell Console (Tools -> JShell Console...)
 // Type or paste them one at a time and observe the output
 
